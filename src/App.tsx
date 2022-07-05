bf66c16fb53d2794d7687c129f03fa3403ca0456
linhexs <838311829@qq.com>
2022-07-06 00:00:17 +0800

    fix: 初始化项目目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./style.scss";
 
-const App: React.FC = () => <div>App</div>;
+const App: React.FC = () => (
+  <div>
+    <a>app</a>
+    <h1>app</h1>
+    <h2>app</h2>
+  </div>
+);
 
 export default App;
